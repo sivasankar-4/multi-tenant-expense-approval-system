1751b2be623855e8719a2ebbdcaf66b50c4f0f42
Siva Sankar <attilisiva459@gmail.com>
2026-07-08 21:56:33 +0530

    first commit

---
+++ expense-approval-system/src/main/java/com/siva/expense_approval_system/ExpenseApprovalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.siva.expense_approval_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseApprovalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseApprovalSystemApplication.class, args);
+	}
+
+}
