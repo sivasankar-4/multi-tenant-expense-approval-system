0d08a050451a451e345096ae4afa04d7e6611a8b
Siva Sankar <attilisiva459@gmail.com>
2026-07-26 21:54:55 +0530

    Added the security dependency

--- expense-approval-system/src/main/java/com/siva/expense_approval_system/infrastructure/security/SecurityConfig.java
+++ expense-approval-system/src/main/java/com/siva/expense_approval_system/infrastructure/security/SecurityConfig.java
@@ -1,8 +1,16 @@
 package com.siva.expense_approval_system.infrastructure.security;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+
+@Configuration
 public class SecurityConfig{
      
-       
+       public PasswordEncoder passwordEncoder() {
+         
+         return new BCryptPasswordEncoder();
+       }
        
 }
