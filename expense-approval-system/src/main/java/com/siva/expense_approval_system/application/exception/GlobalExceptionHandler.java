5899da95b4708431f35a8a06613c3bff90e11992
Siva Sankar <attilisiva459@gmail.com>
2026-08-04 12:52:09 +0530

    Added a exception to handle the version exceptions

--- expense-approval-system/src/main/java/com/siva/expense_approval_system/application/exception/GlobalExceptionHandler.java
+++ expense-approval-system/src/main/java/com/siva/expense_approval_system/application/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -45,4 +46,14 @@ public class GlobalExceptionHandler {
         }
         return errors;
     }
+
+    @ExceptionHandler(ObjectOptimisticLockingFailureException.class)
+public ResponseEntity<Map<String, String>> handleOptimisticLocking(
+        ObjectOptimisticLockingFailureException ex) {
+
+    Map<String, String> error = new HashMap<>();
+    error.put("error", "This expense was modified by another user. Please refresh and try again.");
+
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+}
 }
