c7555a8914ad7ec304a5e0233df95f37ea3c84f0
Siva Sankar <attilisiva459@gmail.com>
2026-08-14 14:37:20 +0530

    updated table name

--- src/main/java/com/siva/expense_approval_system/domain/model/ApprovalAction.java
+++ src/main/java/com/siva/expense_approval_system/domain/model/ApprovalAction.java
@@ -18,7 +18,7 @@ import jakarta.persistence.UniqueConstraint;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "approvalAction", uniqueConstraints = @UniqueConstraint(
+@Table(name = "approval_action", uniqueConstraints = @UniqueConstraint(
         name = "uk_approval_action_expense_step",
         columnNames = {"expense_id", "workflow_step"}))
 @NoArgsConstructor
