7dc025136d2e66dae685a29cf8ee8ed079c3b964
Siva Sankar <attilisiva459@gmail.com>
2026-08-26 23:13:34 +0530

    added signup request

---
+++ src/main/java/com/siva/expense_approval_system/api/dto/request/SignUpRequest.java
@@ -0,0 +1,54 @@
+package com.siva.expense_approval_system.api.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class SignUpRequest {
+
+    @NotBlank(message = "Company name is required")
+    private String companyName;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must contain at least 8 characters")
+    private String password;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
