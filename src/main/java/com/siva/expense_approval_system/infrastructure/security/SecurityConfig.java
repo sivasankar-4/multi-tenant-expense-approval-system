5c54ddb7c3cdb16a70f92eb183f58f4fcf30a9d0
Siva Sankar <attilisiva459@gmail.com>
2026-09-04 19:39:48 +0530

    updated the frontend url

--- src/main/java/com/siva/expense_approval_system/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/siva/expense_approval_system/infrastructure/security/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:5173" ,"http://localhost:5174","http://localhost:5175"));
+    configuration.setAllowedOrigins(List.of("http://localhost:5173" ,"http://localhost:5174","http://localhost:5175","https://expense-approval-system-frontend-roan.vercel.app"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowedHeaders(List.of(
         HttpHeaders.AUTHORIZATION,
